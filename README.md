96b1c6d90b37b81bcdf49d9b48325b4847902b50
gaotuan <279264044@qq.com>
2020-04-02 17:02:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,102 @@
+<p align="center">
+        <img width="300" src="logo.png">
+</p>
+
+# Yearning SQL审核平台
+
+![](https://img.shields.io/badge/build-release-brightgreen.svg)  
+![](https://img.shields.io/badge/version-v1.2.0-brightgreen.svg)  
+![](https://img.shields.io/badge/vue.js-2.5.0-brightgreen.svg) 
+![](https://img.shields.io/badge/iview-2.8.0-brightgreen.svg?style=flat-square) 
+![](https://img.shields.io/badge/python-3.6-brightgreen.svg)
+![](https://img.shields.io/badge/Django-2.0-brightgreen.svg)
+
+##### 基于Inception的企业级web SQL审核平台。
+
+## Website 官网
+
+[www.yearning.io](http://yearning.io)
+
+## Feature 功能
+
+- 数据库字典自动生成
+- SQL查询
+    - 查询工单 
+    - 导出
+    - 自动补全，智能提示 
+- SQL可视化自动生成
+    - 索引语句自动生成
+    - DDL语句自动生成
+- SQL审核
+    - 流程化工单
+    - SQL语句检测
+    - SQL语句执行
+    - SQL回滚
+    - 历史审核记录
+    - 查询语句审计
+- 推送
+    - 站内信工单通知
+    - E-mail工单推送
+    - 钉钉webhook机器人工单推送
+- 其他
+    - todoList
+    - LDAP登陆   
+- 用户权限及管理
+    - 拼图式权限划分(共12项独立权限,可随意组合)
+
+## Environment 环境
+
+- Python 3.6
+
+- Vue.js 2.5
+
+- Django 2.0
+
+## Install 安装及使用日志
+
+详细安装步骤请访问[www.yearning.io](http://yearning.io)获得帮助
+
+[使用及安装文档](http://supermancookie.com/Yearning-document/)
+
+## Support 支持Yearning
+
+如果Yearning能够帮助到你，请支持下Yearning吧，让Yearning能够持续改善并更新
+
+![](http://oy0f4k5qi.bkt.clouddn.com/alipay.jpg)
+![](http://oy0f4k5qi.bkt.clouddn.com/wechat.jpg)
+  
+## About 联系方式
+   
+   QQ群:103674679
+   
+   E-mail: im@supermancookie.com
+
+## Snapshot 效果展示
+
+- Login
+
+![login](http://oy0f4k5qi.bkt.clouddn.com/login_yearning.png)
+
+
+- Dashboard
+
+![](http://oy0f4k5qi.bkt.clouddn.com/23123.png)
+
+- SQL语法高亮及自动补全
+
+![](http://oy0f4k5qi.bkt.clouddn.com/dml.png)
+
+- 细粒度的权限分配
+![](http://oy0f4k5qi.bkt.clouddn.com/per.png)
+
+- 我的工单
+![](http://oy0f4k5qi.bkt.clouddn.com/myorder.png)
+
+
+## License
+
+- Apache 2.0
+
+2018 © Cookie
+
+
---
+++ src/core/api/user.py
@@ -0,0 +1,338 @@
+import logging
+import json
+from libs import baseview, util
+from core.task import grained_permissions
+from libs.serializers import UserINFO
+from rest_framework.response import Response
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.db import transaction
+from rest_framework_jwt.settings import api_settings
+from core.models import (
+    Account,
+    Usermessage,
+    Todolist,
+    grained
+)
+
+CUSTOM_ERROR = logging.getLogger('Yearning.core.views')
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+PERMISSION = {
+    'ddl': '0',
+    'ddlcon': [],
+    'dml': '0',
+    'dmlcon': [],
+    'dic': '0',
+    'diccon': [],
+    'dicedit': '0',
+    'user': '0',
+    'base': '0',
+    'dicexport': '0',
+    'person': [],
+    'query': '0',
+    'querycon': ''
+}
+
+
+class userinfo(baseview.BaseView):
+    '''
+        User Management interface
+
+        mothod：
+
+        get:
+
+            get all user information, a page consists of 20 user info
+
+        put:
+
+            if args equal to changepwd (/api/v1/userinfo/changepwd) change the password
+
+            if args equal to changegroup (/api/v1/userinfo/changegroup) change the group
+
+        post: 
+   
+            add user
+
+        delete:
+   
+            del user
+      
+    '''
+
+    def get(self, request, args=None):
+        if args == 'all':
+            try:
+                page = request.GET.get('page')
+            except KeyError as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                return HttpResponse(status=500)
+            else:
+                try:
+                    page_number = Account.objects.count()
+                    start = int(page) * 10 - 10
+                    end = int(page) * 10
+                    info = Account.objects.all()[start:end]
+                    serializers = UserINFO(info, many=True)
+                    return Response({'page': page_number, 'data': serializers.data})
+                except Exception as e:
+                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                    return HttpResponse(e)
+
+        elif args == 'permissions':
+            user = request.GET.get('user')
+            user = grained.objects.filter(username=user).first()
+            return Response(user.permissions)
+
+    def put(self, request, args=None):
+        if args == 'changepwd':
+            try:
+                username = request.data['username']
+                new_password = request.data['new']
+            except KeyError as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                return HttpResponse(status=500)
+            else:
+                try:
+                    user = Account.objects.get(username__exact=username)
+                    user.set_password(new_password)
+                    user.save()
+                    return Response('%s--密码修改成功!' % username)
+                except Exception as e:
+                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                    return HttpResponse(status=500)
+
+        elif args == 'changegroup':
+            try:
+                username = request.data['username']
+                group = request.data['group']
+                department = request.data['department']
+                permission = json.loads(request.data['permission'])
+                brfore = Account.objects.filter(username=username).first()
+            except KeyError as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                return HttpResponse(status=500)
+            else:
+                try:
+                    if brfore.group == 'admin' and group == 'guest':
+                        per = grained.objects.all().values('username', 'permissions')
+                        for i in per:
+                            for c in i['permissions']:
+                                if isinstance(i['permissions'][c], list) and c == 'person':
+                                    i['permissions'][c] = list(filter(lambda x: x != username, i['permissions'][c]))
+                            grained.objects.filter(username=i['username']).update(permissions=i['permissions'])
+                    grained.objects.filter(username=username).update(permissions=permission)
+                    if group == 'admin' or group == 'perform':
+                        Account.objects.filter(username=username).update(
+                            group=group,
+                            department=department,
+                            is_staff=1
+                        )
+                    else:
+                        Account.objects.filter(username=username).update(
+                            group=group,
+                            department=department,
+                            is_staff=0
+                        )
+                    return Response('%s--权限修改成功!' % username)
+                except Exception as e:
+                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                    return HttpResponse(status=500)
+
+        elif args == 'changemail':
+            try:
+                username = request.data['username']
+                mail = request.data['mail']
+            except KeyError as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                return HttpResponse(status=500)
+            else:
+                try:
+                    Account.objects.filter(username=username).update(email=mail)
+                    return Response('%s--E-mail修改成功!' % username)
+                except Exception as e:
+                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                    return HttpResponse(status=500)
+
+    def post(self, request, args=None):
+        try:
+            username = request.data['username']
+            password = request.data['password']
+            group = request.data['group']
+            department = request.data['department']
+            email = request.data['email']
+        except KeyError as e:
+            CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+            return HttpResponse(status=500)
+        else:
+            try:
+                if group == 'admin' or group == 'perform':
+                    user = Account.objects.create_user(
+                        username=username,
+                        password=password,
+                        department=department,
+                        group=group,
+                        is_staff=1,
+                        email=email)
+                    user.save()
+                    grained.objects.get_or_create(username=username, permissions=PERMISSION)
+                    return Response('%s 用户注册成功!' % username)
+                elif group == 'guest':
+                    user = Account.objects.create_user(
+                        username=username,
+                        password=password,
+                        department=department,
+                        group=group,
+                        email=email
+                    )
+                    user.save()
+                    grained.objects.get_or_create(username=username, permissions=PERMISSION)
+                    return Response('%s 用户注册成功!' % username)
+            except Exception as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                return Response(e)
+
+    def delete(self, request, args=None):
+        try:
+            pr = Account.objects.filter(username=args).first()
+            if pr.is_staff == 1:
+                per = grained.objects.all().values('username', 'permissions')
+                for i in per:
+                    for c in i['permissions']:
+                        if isinstance(i['permissions'][c], list) and c == 'person':
+                            i['permissions'][c] = list(filter(lambda x: x != args, i['permissions'][c]))
+                    grained.objects.filter(username=i['username']).update(permissions=i['permissions'])
+            with transaction.atomic():
+                Account.objects.filter(username=args).delete()
+                Usermessage.objects.filter(to_user=args).delete()
+                Todolist.objects.filter(username=args).delete()
+                grained.objects.filter(username=args).delete()
+            return Response('%s--用户已删除!' % args)
+        except Exception as e:
+            CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+            return HttpResponse(status=500)
+
+
+class generaluser(baseview.BaseView):
+    '''
+
+    :argument 普通用户修改密码
+
+    '''
+
+    def post(self, request, args=None):
+        if args == 'changepwd':
+            try:
+                username = request.data['username']
+                old_password = request.data['old']
+                new_password = request.data['new']
+            except KeyError as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+            else:
+                try:
+                    user = authenticate(username=username, password=old_password)
+                    if user is not None and user.is_active:
+                        user.set_password(new_password)
+                        user.save()
+                        return Response('%s--密码修改成功!' % username)
+                    else:
+                        return Response('%s--原密码不正确请重新输入' % username)
+                except Exception as e:
+                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                    return HttpResponse(status=500)
+
+    def put(self, request, args: str = None):
+        try:
+            mail = request.data['mail']
+        except KeyError as e:
+            CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+        else:
+            try:
+                Account.objects.filter(username=request.user).update(email=mail)
+                return Response('邮箱地址已更新!')
+            except Exception as e:
+                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+                return HttpResponse(status=500)
+
+
+class authgroup(baseview.BaseView):
+    '''
+
+    认证组权限
+
+    '''
+
+    @grained_permissions
+    def post(self, request, args=None):
+        try:
+            _type = request.data['permissions_type'] + 'edit'
+            permission = grained.objects.filter(username=request.user).first()
+            return Response(permission.permissions[_type])
+        except Exception as e:
+            CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+            return HttpResponse(status=500)
+
+
+class ldapauth(baseview.AnyLogin):
+    '''
+
+    ldap用户认证
+
+    '''
+
+    def post(self, request, args: str = None):
+        try:
+            username = request.data['username']
+            password = request.data['password']
+        except KeyError as e:
+            CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+        else:
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+            valite = util.auth(username=username, password=password)
+            if valite:
+                try:
+                    user = Account.objects.filter(username=username).first()
+                    user.set_password(password)
+                    user.save()
+                    payload = jwt_payload_handler(user)
+                    token = jwt_encode_handler(payload)
+                    return Response({'token': token, 'res': '', 'permissions': user.group})
+                except:
+                    permissions = Account.objects.create_user(
+                        username=username,
+                        password=password,
+                        is_staff=0,
+                        group='guest')
+                    permissions.save()
+                    grained.objects.get_or_create(username=username, permissions=PERMISSION)
+                    _user = authenticate(username=username, password=password)
+                    token = jwt_encode_handler(jwt_payload_handler(_user))
+                    return Response({'token': token, 'res': '', 'permissions': 'guest'})
+            else:
+                return Response({'token': 'null', 'res': 'ldap账号认证失败,请检查ldap账号或ldap配置!'})
+
+
+class login_auth(baseview.AnyLogin):
+
+    def post(self, request, args: str = None):
+
+        '''
+        普通登陆类型认证
+        :return: jwt token
+        '''
+
+        try:
+            user = request.data['username']
+            password = request.data['password']
+        except KeyError as e:
+            CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
+        else:
+            permissions = authenticate(username=user, password=password)
+            if permissions is not None and permissions.is_active:
+                token = jwt_encode_handler(jwt_payload_handler(permissions))
+                return Response({'token': token, 'res': '', 'permissions': permissions.group})
+            else:
+                return HttpResponse(status=400)
